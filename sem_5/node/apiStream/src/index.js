d8099e718ad03dd5e22b870833a82d16dc76671e
frannois <francoistoyi4@gmail.com>
2024-11-08 09:31:07 +0000

    authentification ok

--- sem_5/node/apiStream/src/index.js
+++ sem_5/node/apiStream/src/index.js
@@ -2,6 +2,31 @@ import express from "express"
 import UserRouter from "./routes/UserRouter.js";
 import VideosRouter from "./routes/VideosRouter.js";
 import AuthRouter from "./routes/AuthRouter.js";
+// import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import authMiddlware from "./middleware/authMiddleware.js"
+
+dotenv.config();
+
+// const JWT_SECRET = process.env.JWT_SECRET || 'secret_key';
+
+// const authMiddlware = (req,res,next) => {
+//     // console.log(req.originalUrl);
+    
+//     if (req.originalUrl != '/auth/login') {
+//         // console.log("uuuuuuuuuuuuuuuuuuuuu");
+//         const token = req.headers['authorization']?.split(' ')[1]
+//         if (!token) return res.status(401).json({ message: 'Accès refusé' });
+//         jwt.verify(token, JWT_SECRET, (err, user) => {
+//             if (err) return res.status(403).json({ message: 'Token invalide' });
+//             req.user = user;
+//             next();
+//         });
+//     }
+//     // console.log(token);
+//     next()
+// }
+
 
 
 // console.log(UserRouter);
@@ -17,6 +42,7 @@ const userRouter = new UserRouter();
 const videoRouter = new VideosRouter();
 const authRouter = new AuthRouter();
 
+app.use(authMiddlware)
 
 app.use(express.json())
 app.use('/users',userRouter.getRouter());
--- sem_5/node/apiStream/src/middleware/authMiddleware.js
+++ sem_5/node/apiStream/src/middleware/authMiddleware.js
@@ -5,12 +5,20 @@ dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secret_key';
 
-export function authenticateToken(req, res, next) {
-    const token = req.headers['authorization']?.split('')[1];
-    if (!token) return res.status(401).json({ message: 'Accès refusé' });
+export default function authenticateToken(req, res, next) {
+    if (req.originalUrl != '/auth/login') {
+        const token = req.headers['authorization']?.split(' ')[1];
+        // console.log(token);
+        
+        if (!token) return res.status(401).json({ message: 'Accès refusé' });
         jwt.verify(token, JWT_SECRET, (err, user) => {
             if (err) return res.status(403).json({ message: 'Token invalide' });
             req.user = user;
             next();
-            });
+        });
+    }
+    else{
+        next()
+    }
+    // return next()
 }
