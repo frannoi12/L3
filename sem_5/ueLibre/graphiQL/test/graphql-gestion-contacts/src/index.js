fcd59040faa71726fd6a4e67b5d1f97e808e1dcd
frannoi12 <francoistoyi4@gmail.com>
2025-01-27 23:04:03 +0000

    just peace modification

--- sem_5/ueLibre/graphiQL/test/graphql-gestion-contacts/src/index.js
+++ sem_5/ueLibre/graphiQL/test/graphql-gestion-contacts/src/index.js
@@ -1,12 +1,11 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone'; // Importer la méthode pour un serveur autonome
-import typeDefs from './schema.js';
-import resolvers from './resolvers.js';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone"; // Importer la méthode pour un serveur autonome
+import typeDefs from "./schema.js";
+import resolvers from "./resolvers.js";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  playground : true,
 });
 
 // Démarre le serveur autonome
